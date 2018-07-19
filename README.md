46f5a6b772eb5f1a3689a737b01286567ea40379
Dalia <daliabehiry@gmail.com>
2018-07-19 21:07:42 +0200

    RestaurantReviews-Project repo

---
+++ README.md
@@ -0,0 +1,46 @@
+# Restaurant Reviews App
+
+## About Project
+
+- The purpose of this project is to navigate to restaurants by determining their locations on the map. By choosing a location, the user will get the names of restaurants found there, their addresses, images, work times and even reviews if available. User can choose the restaurant by the location or the type of cuisine .
+- The project concentrates on two aspects: adding responsiveness with accessibility and using service worker to enable user's use of application offline.
+
+## Project installation
+
+1- Download the repo:
+
+[]: <https://github.com/Dalia-Yahia/Udacity-RestaurantReviews-Project>
+
+2- Run the server by open Git Bash inside the folder of the project and write:
+
+```
+python -m http.server 8000
+```
+
+be sure you have python on your device or install it .
+
+3- go to your browser and write
+
+```
+localhost:8000
+```
+
+and BING the Restaurant Reviews project
+
+## Project features
+
+### Responsiveness :
+
+- The app is responsive to all of different device screens.
+
+### Accessibility :
+
+- The app has accessibility features which enable users with some impairments to navigate easily through the app.
+
+### Offline mode :
+
+- If the user loses internet connection for any reason , the app will turn to work on offline mode and it will show the all assets files that app is needed
+
+### Project dependencies :
+
+- Mapbox for map
---
+++ sw.js
@@ -0,0 +1,75 @@
+//start chaching project files
+var CACHE_NAME = 'restaurant-cache1';
+
+var allFiles = [
+  "/",
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/img/13.jpg',
+  '/img/26.jpg',
+  '/img/logo.png',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/js/dbhelper.js',
+];
+
+self.addEventListener('install', function(event) {
+  //start install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+          console.log('opened cache');
+            return cache.addAll(allFiles);
+        })
+    );
+});
+
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != CACHE_NAME;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          }
+          var fetchRequest = event.request.clone();
+          return fetch(fetchRequest).then(function (response){
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            var responseToChache = response.clone();
+            caches.open(CACHE_NAME).then(function (chache) {
+              chache.put(event.request, responseToChache);
+            });
+            return response;
+          }
+        );
+      })
+    );
+})
